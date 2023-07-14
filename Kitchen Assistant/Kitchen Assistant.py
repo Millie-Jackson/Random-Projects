5183fdecc748a35e3c74e8eca2d2bd372b3cdade
Millie Jackson <100158073+Millie-Jackson@users.noreply.github.com>
2023-07-14 13:03:47 +0100

    Compares pantry ingredients quantities to a specific recipe

--- Kitchen Assistant/Kitchen Assistant.py
+++ Kitchen Assistant/Kitchen Assistant.py
@@ -3,50 +3,115 @@ def price_per_gram(price, quantity):
     total = price / quantity
     return f"{round(total, 2)}g"
 
-# Asks how much of an ingredient the user has and updates the quantity in the pantry
-def quantity_check(ingredient):
+# Updates the quantity of a specific ingredient in the pantry
+def quantity_check(pantry, ingredient):
 
-    '''Asks how much of an ingredient the user has
-    Asks the user for input
-    Changes the quantity in the pantry'''
+    """
+    Updates the quantity of a specific ingredient in the pantry.
+
+    Args:
+        pantry (list): A list of dictionaries representing the items available in the pantry.
+        ingredient (dict): A dictionary representing the ingredient with its name, price, quantity, and stock status.
+
+    Returns:
+        None
+    """
 
     ingredient_name = ingredient["Name"]
-    quantity = input(f"How much of {ingredient_name} have you got?")
-    bell_pepper["Quantity"] = quantity
+    quantity = input(f"How many grams of {ingredient_name} do you have?")
+
+    for item in pantry:
+        if item["Name"] == ingredient_name:
+            item["Quantity"] = int(quantity)
+            break
 
     return None
 
-bell_pepper = {"Name": "Bell Pepper", "Price": 1.35, "Quantity": 900, "In Stock": False}
-gnocchi = {"Name": "Gnocchi", "Price": 2.00, "Quantity": 900, "In Stock": False}
-meatballs = {"Name": "Meatballs", "Price": 1.75, "Quantity": 300, "In Stock": False}
-#bell_pepper = price_per_gram(1.35, 600)
-#gnocchi =  price_per_gram(2.00, 500)
-#meatballs = price_per_gram(1.75, 300)
-tomato = price_per_gram(0.32, 400)
-soft_cheese = price_per_gram(3.00, 130)
-parsley = price_per_gram(0.55, 30)
-garlic = price_per_gram(1.25, 300)
-nutritional_yeast = price_per_gram(3.00, 100)
-rosemary = price_per_gram(0.60, 27)
-salt = price_per_gram(1.25, 350)
-black_pepper = price_per_gram(0.90, 25)
-chili = price_per_gram(0.70, 32)
-pantry = [bell_pepper, gnocchi, meatballs]
+# Compares the availability of ingredients in the pantry to the amount needed in a recipe
+def compare_pantry_to_recipe(pantry, recipe):
 
-recipe_book = {"Name": "Roasted Gnocchi", 
-               "Servings": 3, 
-               "Ingredients": {"Bell pepper": 600, 
-                               "Gnocchi": 500, 
-                                "Meatballs": 300, 
-                                "Tomato": 300, 
-                                "Soft Cheese": 60, 
-                                "Parsley": 30, 
-                                "Garlic": 2, 
-                                "Nutritional yeast": f"{3}tbsp", 
-                                "Rosemary": f"{2}tbsp", 
-                                "Salt": f"{1}tsp", 
-                                "Black pepper": f"{0.5}tsp", 
-                                "Chili": f"{0.75}tsp"}}
+    """
+    Compares the availability of ingredients in the pantry to the amount needed in a recipe.
+
+    Args:
+        pantry (list): A list of dictionaries representing the items available in the pantry.
+        recipe (dict): A dictionary representing the recipe with its name, servings, and ingredients.
+
+    Returns:
+        list: A list containing a dictionary with the recipe name as the key and the ingredient comparison as the value.
+
+    """
+
+    ingredient_comparison = {}
+
+    for ingredient, amount_needed in recipe["Ingredients"].items():
+        for item in pantry:
+            if item["Name"] == ingredient:
+                ingredient_comparison[ingredient] = "In Stock"
+                if item["Quantity"] < amount_needed:
+                    ingredient_comparison[ingredient] = "Not Enough"
+                break
+            else:
+                ingredient_comparison[ingredient] = "Not In Stock"
+
+    return [{recipe["Name"]: ingredient_comparison}]
+
+pantry = [
+    {"Name": "Gnocchi", "Price": 2.00, "Quantity": 900, "In Stock": False},
+    {"Name": "Udon Noodles", "Price": 2.00, "Quantity": 900, "In Stock": False},
+    {"Name": "Meatballs", "Price": 1.75, "Quantity": 300, "In Stock": False},
+    {"Name": "Bell Pepper", "Price": 1.35, "Quantity": 900, "In Stock": False},
+    {"Name": "Mushroom", "Price": 1.35, "Quantity": 900, "In Stock": False},
+    {"Name": "Tomato", "Price": 0.32, "Quantity": 400, "In Stock": False},
+    {"Name": "Scallions", "Price": 0.32, "Quantity": 400, "In Stock": False},
+    {"Name": "Edamame", "Price": 0.32, "Quantity": 400, "In Stock": False},
+    {"Name": "Cream Cheese", "Price": 3.00, "Quantity": 130, "In Stock": False},
+    {"Name": "Butter", "Price": 0.32, "Quantity": 400, "In Stock": False},
+    {"Name": "Soy Sauce", "Price": 0.32, "Quantity": 400, "In Stock": False},
+    {"Name": "Parsley", "Price": 0.55, "Quantity": 30, "In Stock": False},
+    {"Name": "Garlic", "Price": 1.25, "Quantity": 300, "In Stock": False},
+    {"Name": "Rosemary", "Price": 0.60, "Quantity": 27, "In Stock": False},
+    {"Name": "Salt", "Price": 1.25, "Quantity": 350, "In Stock": False},
+    {"Name": "Black Pepper", "Price": 0.90, "Quantity": 25, "In Stock": False},
+    {"Name": "Garlic Powder", "Price": 0.32, "Quantity": 400, "In Stock": False},
+    {"Name": "Chili", "Price": 0.70, "Quantity": 32, "In Stock": False},
+    {"Name": "Nutritional Yeast", "Price": 3.00, "Quantity": 100, "In Stock": False}
+]
+
+# Everything is in grams
+recipe_book = [
+    {
+        "Name": "Roasted Gnocchi",
+        "Servings": 3,
+        "Ingredients": {
+            "Bell pepper": 600,
+            "Gnocchi": 500,
+            "Meatballs": 300,
+            "Tomato": 300,
+            "Soft Cheese": 60,
+            "Parsley": 30,
+            "Garlic": 2,
+            "Nutritional yeast": 15,
+            "Rosemary": 10,
+            "Salt": 5,
+            "Black pepper": 7,
+            "Chili": 3
+        }
+    },
+    {
+        "Name": "Shoyu Butter Udon Noodles",
+        "Servings": 2,
+        "Ingredients": {
+            "Udon Noodles": 400,
+            "Mushrooms": 200,
+            "Edamame": 100,
+            "Butter": 30,
+            "Scallions": 20,
+            "Soy Sauce": 15,
+            "Garlic Powder": 3
+        }
+    }
+]
 
 # CURRENTLY WORKING ON THIS
 # Need to remove the g
@@ -67,23 +132,6 @@ def recipe_check(recipe):
             pass
     return None
 
-
-def compare_pantry_to_recipe(recipe):
-    for i in recipe.get("Ingredients"):
-        print(i)
-        for j in pantry:
-            pass
-                #if i >= j:
-                #    i["In Stock"] = True
-                #else:
-                #    i["In Stock"] = False
-    #print(i)
-    #print(j)
-
-
- 
-
-compare_pantry_to_recipe(recipe_book)
 ## TO DO ##
 # Use a data class
 # Ingredients inherit from a class?
@@ -93,6 +141,7 @@ compare_pantry_to_recipe(recipe_book)
 # Allow user to enter their own recipes
 # Allows user to enter their shopping
 # Asks the user how much of an ingredient they have in order to fulfill the top 3 recipes
+# Suggests a list of the top 3 most common ingredients needed (based on ingredieant frequency) 
 
 # Function that suggests ingredient swaps to reduce recipe price
 # Ingredient price changes with the season (4 prices per item?)
