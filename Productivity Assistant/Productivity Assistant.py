20f91b510fe06d2fc3d97b85f8b76ed442968a25
Millie Jackson <100158073+Millie-Jackson@users.noreply.github.com>
2023-07-23 21:11:43 +0100

    main() and init() setup

--- Productivity Assistant/Productivity Assistant.py
+++ Productivity Assistant/Productivity Assistant.py
@@ -1,3 +1,46 @@
+import time
+
+
+
+def set_timer() -> None:
+
+    while True:
+        try:
+            seconds = int(input("How long would you like to work for?"))
+            if seconds <= 0:
+                print("Oops! Time only flows forward")
+            else:
+                break
+        except ValueError:
+            print("Huh? What do you mean? Please enter a time")
+
+    print(f"Timer set for {seconds} seconds")
+    time.sleep(seconds)
+    print("Time's up!")
+
+    return None 
+
+
+
+def main() -> None:
+
+    while True:
+        response = input("Do you want to set a work session?")
+        if response.startswith("y" or "Y"):
+            set_timer()
+        else:
+            print("Quit")
+            break
+
+    return None
+
+
+
+if __name__ == "__main__":
+    main()
+
+
+
 ### END OF FILE ###
 
 
@@ -29,6 +72,7 @@
 # Set timer
 # Set timer for short (1 pom), medium (2 poms) or long (3 poms)
 # Play noise when pom is finished
+# the time.sleep() function will halt the program execution for the specified duration, so be mindful when using it in your code. If you want to perform other actions while the timer is running, you can consider using threading or asynchronous programming. 
 
 # SECOND BRAIN #
 
@@ -38,5 +82,12 @@
 # Tasks can be grouped by study subject or catagory
 
 
+
+### COMPLETED ###
+
+# POMIDOR TIMER #
+# Ask the user if they want to set a timer
+# Ask for user input
+# Set timer 
 
 
