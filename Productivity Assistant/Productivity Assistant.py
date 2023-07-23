276ebf5ab689f2b74e4fbd31fa8d22e391265858
Millie Jackson <100158073+Millie-Jackson@users.noreply.github.com>
2023-07-23 20:29:44 +0100

    Project set up and initial to do list

---
+++ Productivity Assistant/Productivity Assistant.py
@@ -0,0 +1,42 @@
+### END OF FILE ###
+
+
+
+### TO DO ###
+
+# CALENDAR #
+# Add tasks in pomidors
+# Brakes are added at the end of pomidors
+# Tasks can be automatically allocated to calendar based on carcadian rythem
+# Tasks can be automatically allocated to calendar based on time to complete
+# Suggest afternoon slump
+# Suggest meal times
+# Suggest bedtime
+# Suggest waketime
+
+# NOTES #
+# Research how to take good notes
+# Templates
+    # Meeting notes template
+    # Class notes template
+    # Programming schematic template
+# Incorporate SEN tactics
+    # Spell checker
+    # Grammer checker
+    # Text to speech
+
+# POMIDOR TIMER #
+# Set timer
+# Set timer for short (1 pom), medium (2 poms) or long (3 poms)
+# Play noise when pom is finished
+
+# SECOND BRAIN #
+
+# TASK LIST # 
+# Tasks can be allocated an esimated pom time
+# Tasks can be allocated a difficulty
+# Tasks can be grouped by study subject or catagory
+
+
+
+
