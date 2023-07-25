13af6e2f700f1bf05c9f7086863b1900cbe85d17
Millie Jackson <100158073+Millie-Jackson@users.noreply.github.com>
2023-07-25 11:08:40 +0100

    Set timer for short (1 pom), medium (2 poms) or long (3 poms)

--- Productivity Assistant/Productivity Assistant.py
+++ Productivity Assistant/Productivity Assistant.py
@@ -2,31 +2,44 @@ import time
 
 
 
-def set_timer() -> None:
+def set_timer(minutes) -> None:
 
-    while True:
-        try:
-            seconds = int(input("How long would you like to work for?"))
-            if seconds <= 0:
-                print("Oops! Time only flows forward")
-            else:
-                break
-        except ValueError:
-            print("Huh? What do you mean? Please enter a time")
-
-    print(f"Timer set for {seconds} seconds")
-    time.sleep(seconds)
+    print(f"Timer set for {minutes} minutes")
+    #time.sleep(minutes * 60)
     print("Time's up!")
 
     return None 
 
 
 
+def get_pomidor() -> int:
+
+    while True:
+        try:
+            option = int(input("How many pomidors would you like to work?"))
+            if option in[1, 2, 3]:
+                print(f"Setting timer for {option} poms")
+                if option == 1:
+                    set_timer(25)
+                elif option == 2:
+                    set_timer(50)
+                elif option == 3:
+                    set_timer(75)
+                return option
+            else:
+                print("Huh? Tell me again? 1, 2 or 3?")
+        except ValueError:
+            print("Huh? Tell me again? 1, 2 or 3?")
+
+    return int
+
+
+
 def main() -> None:
 
     while True:
-        response = input("Do you want to set a work session?")
-        if response.startswith("y" or "Y"):
+        response = input("Do you want to set a work session?").lower()
+        if response.startswith("y"):
             set_timer()
         else:
             print("Quit")
@@ -37,7 +50,8 @@ def main() -> None:
 
 
 if __name__ == "__main__":
-    main()
+    #main()
+    pomidor_duration = get_pomidor()
 
 
 
@@ -69,9 +83,8 @@ if __name__ == "__main__":
     # Text to speech
 
 # POMIDOR TIMER #
-# Set timer
-# Set timer for short (1 pom), medium (2 poms) or long (3 poms)
 # Play noise when pom is finished
+# Set a custom time
 # the time.sleep() function will halt the program execution for the specified duration, so be mindful when using it in your code. If you want to perform other actions while the timer is running, you can consider using threading or asynchronous programming. 
 
 # SECOND BRAIN #
@@ -80,14 +93,26 @@ if __name__ == "__main__":
 # Tasks can be allocated an esimated pom time
 # Tasks can be allocated a difficulty
 # Tasks can be grouped by study subject or catagory
+# Automatic Eisenhower matrix
 
+# UI #
+# Ask questions in a text box
+# Display the timer on screen
+# Tasks can be viewed in an Eisenhower matrix
 
 
 ### COMPLETED ###
 
 # POMIDOR TIMER #
-# Ask the user if they want to set a timer
-# Ask for user input
+# Asks the user if they want to set a timer
+# Asks for user input
 # Set timer 
+# Asks the user how many pomidors
+# Set timer for short (1 pom), medium (2 poms) or long (3 poms)
 
 
+
+# EDGE CASES #
+# try-except added to set_timer to only accept ints
+# Text input coverted to lower to account for capital letters
+# try-except added to get_pomidors to only accept ints
